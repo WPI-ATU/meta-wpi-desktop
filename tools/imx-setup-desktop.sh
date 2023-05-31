7e08d02d91035ce9b771fac892c0636054c72cd8
Jackson Wang <xpire0215@gmail.com>
2023-05-31 14:15:15 +0800

    Update imx-setup-desktop.sh

--- tools/imx-setup-desktop.sh
+++ tools/imx-setup-desktop.sh
@@ -1,7 +1,7 @@
 # Override the EULA defined in meta-imx
 cp sources/meta-nxp-desktop/EULA.txt sources/meta-imx/EULA.txt
 
-source sources/meta-imx/tools/imx-setup-release.sh $@
+source sources/meta-wpi/tools/imx-setup-release.sh $@
 
 if [ -f conf/local.conf ]; then
     sed -E "s/(MACHINE \?\?= ')(.*)'/\1\2desktop'/g" -i conf/local.conf
